91db9912706a2f860f1dd998ee8221e13bde6434
vi2920va <vi2920va@gmail.com>
2021-10-01 21:50:48 +0900

    Modify: index.js import 순서 수정

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import './styles/reset.scss';
 import './styles/common.scss';
 import App from './pages/App';
+
 ReactDOM.render(<App />, document.getElementById('root'));
