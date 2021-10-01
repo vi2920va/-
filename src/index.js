bcd1fa75a99d14a24d38bba1630afc471e63f460
vi2920va <vi2920va@gmail.com>
2021-10-01 17:04:43 +0900

    Modify: index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/reset.scss';
+import './styles/common.scss';
 import App from './pages/App';
-
 ReactDOM.render(<App />, document.getElementById('root'));
