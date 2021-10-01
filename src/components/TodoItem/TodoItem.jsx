14fd0e9559608d7aff51a6b9c703879be433e52c
vi2920va <vi2920va@gmail.com>
2021-10-01 10:19:37 +0900

    Add: 1st Redux Core Todo 초기세팅

---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoItem = () => {
+  return <div>TodoITem</div>;
+};
+
+export default TodoItem;
---
+++ src/pages/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TodoList from '../components/TodoList/TodoList';
+
+// git branch -m main
+const App = () => {
+  return <TodoList />;
+};
+
+export default App;
