2f6e15d6bf48cc29020eab93a90753ee27904f65
vi2920va <vi2920va@gmail.com>
2021-10-01 21:47:52 +0900

    Add: TodoList 레이아웃 스타일링 - TodoList 스타일링을 위한 마크업 완료. - TodoList 스타일 적용중

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import './TodoList.scss';
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <main className="todo">
+      <div className="todo-container">
+        <h1 className="todo-title">
+          Todo List
+          <span className="todo-explanation">
+            Get things done. one item a time.
+          </span>
+        </h1>
+
+        <ul className="todo-list">
+          <li>
+            <span>Code a todo list</span>
+            <input type="checkbox" aria-label="todo checkbox value" />
+            <button type="button" className="delete-btn">
+              <i className="fas fa-trash" />
+            </button>
+          </li>
+        </ul>
+
+        <div className="toggle-btn-wrapper">
+          <span>Move done items at the end?</span>
+          <input type="checkbox" aria-label="toggle btn checkbox" />
+        </div>
+
+        <form className="add-form">
+          <h2>Add to the todo list</h2>
+          <input
+            type="text"
+            className="add-input"
+            aria-label="add input text"
+          />
+          <button type="submit" className="add-btn" name="ADD ITEM" />
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default TodoList;
