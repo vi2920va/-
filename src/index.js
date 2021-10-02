e29deaa5f344774c6bbaf2043fa99c04880175e9
vi2920va <vi2920va@gmail.com>
2021-10-02 19:46:13 +0900

    Modify: index.js store 연결

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import App from './pages/App';
 import './styles/reset.scss';
 import './styles/common.scss';
-import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
