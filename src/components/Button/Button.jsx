cf9c22b0c5aae73e31b22686bcf5d238494491f5
vi2920va <vi2920va@gmail.com>
2021-10-04 08:56:10 +0900

    Add: Button 컴포넌트 분리

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Button = () => {
-  return <div></div>;
+const Button = ({ type = 'button', children, className, onClick }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    onClick(isClicked);
+  };
+  
+  return (
+    <button type={type} className={className} onClick={handleClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
