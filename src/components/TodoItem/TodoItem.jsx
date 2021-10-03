35ff82a06bd3c6ba5323853c016112d5f2816ccb
vi2920va <vi2920va@gmail.com>
2021-10-03 22:12:57 +0900

    Modify: 할 일 수정 & 할 일 삭제 구현

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteTodo, saveTodo } from '../../reducers/todos';
+import './TodoItem.scss';
 
-const TodoItem = () => {
-  return <div>TodoITem</div>;
+const TodoItem = ({ todo, index }) => {
+  const [active, setActive] = useState(false);
+  const [value, setValue] = useState(todo.value || '');
+
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(index));
+  };
+
+  const handleEdit = () => {
+    setActive(true);
+  };
+
+  const handleInput = (event) => {
+    const { value } = event.target;
+    if (event.type === 'change') {
+      setValue(value);
+    }
+  };
+
+  const handleSave = () => {
+    dispatch(saveTodo(Object.assign(todo, { value })), index);
+    setActive(false);
+  };
+
+  return (
+    <li key={todo.id} className="todo-item">
+      {!active && <span>{todo.value}</span>}
+      {active && (
+        <input
+          type="text"
+          value={value}
+          aria-label="todos edit text"
+          onChange={handleInput}
+        />
+      )}
+      <div>
+        <button type="button" className="delete-btn" onClick={handleDelete}>
+          <i className="fas fa-trash" />
+        </button>
+        {!active && (
+          <button type="button" className="edit-btn" onClick={handleEdit}>
+            <i className="fas fa-edit" />
+          </button>
+        )}
+        {active && (
+          <button type="button" className="save-btn" onClick={handleSave}>
+            <i className="fas fa-save" />
+          </button>
+        )}
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
