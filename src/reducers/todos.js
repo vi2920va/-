23f6581f86407f1cad2f4973ac92089b10ef6fb6
vi2920va <vi2920va@gmail.com>
2021-10-03 22:10:43 +0900

    Modify: todos 수정

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,34 +1,28 @@
-const LOAD_TODOS = 'LOAD_TODOS';
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
-
-export const loadToDos = () => ({
-  type: LOAD_TODOS,
-});
+const SAVE_TODO = 'EDIT_TODO';
 
 export const addTodo = (value) => ({
   type: ADD_TODO,
   value,
 });
 
-const deleteTodo = (id) => ({
+export const deleteTodo = (index) => ({
   type: DELETE_TODO,
-  id,
+  index,
 });
 
-const setTodoItem = (todos) => {
-  localStorage.setItem('TODOS', JSON.stringify(todos));
-};
+export const saveTodo = (item, index) => ({
+  type: SAVE_TODO,
+  item,
+  index,
+});
 
 export default function todos(state = [], action) {
+  let list = [...state];
   switch (action.type) {
-    case LOAD_TODOS:
-      return JSON.parse(localStorage.getItem('TODOS') || []);
-      
     case ADD_TODO:
       const id = Date.now().toString();
-      setTodoItem([...state, { id, value: action.value, isCompleted: false }]);
-
       return [
         ...state,
         {
@@ -37,6 +31,14 @@ export default function todos(state = [], action) {
           isCompleted: false,
         },
       ];
+
+    case DELETE_TODO:
+      list.splice(action.index, 1);
+      return list;
+
+    case SAVE_TODO:
+      list.splice(action.index, action.item);
+      return list;
 
     default:
       return state;
