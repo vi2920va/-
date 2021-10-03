11639889007fb28e6741020efacd15705c632586
vi2920va <vi2920va@gmail.com>
2021-10-03 22:11:35 +0900

    Modify: Form 수정

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../reducers/todos';
+import './Form.scss';
 
 const Form = () => {
-  return <div></div>;
+
+  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
+
+  const handleInput = (event) => {
+    const { value } = event.target;
+    setValue(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addTodo(value));
+    setValue('');
+  };
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h2>Add to the todo list</h2>
+      <input
+        type="text"
+        className="add-input"
+        aria-label="add input text"
+        value={value}
+        onChange={handleInput}
+      />
+      <button type="submit" className="add-btn">
+        ADD ITEM
+      </button>
+    </form>
+  );
 };
 
 export default Form;
