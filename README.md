18ebc3719bf403f10a8e5ef251f8fc0115eec147
vi2920va <vi2920va@gmail.com>
2021-10-03 22:13:27 +0900

    Modify: 파일 수정

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Redux Core Todo
+
+![codeTodo](https://i.postimg.cc/13QdQd4c/screencapture-localhost-3002-2021-10-03-22-07-01.png)
+
+Rudux Core를 사용한 간단한 투두 리스트.
+
+## 적용기술
+
+- React(HOOK)
+- Redux & Redux HOOKS
+- SCSS
+
+## 구현 기능
+
+- 할 일 추가
+- 등록된 일 수정
+- 등록된 일 제거
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './pages/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
-import App from './pages/App';
 import './styles/reset.scss';
 import './styles/common.scss';
 
